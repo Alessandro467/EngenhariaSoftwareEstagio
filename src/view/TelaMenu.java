a08f41f6c8bf12683213cb107c9f055853a5f9ec
Alessandro467 <135671702+Alessandro467@users.noreply.github.com>
2023-06-05 20:36:21 -0300

    Add files via upload

---
+++ src/view/TelaMenu.java
@@ -0,0 +1,98 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JDesktopPane;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TelaMenu {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaMenu window = new TelaMenu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaMenu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		
+		JDesktopPane desktopPane = new JDesktopPane();
+		frame.getContentPane().add(desktopPane, BorderLayout.CENTER);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu Registros = new JMenu("Registros");
+		menuBar.add(Registros);
+		
+		JMenuItem mntmDocumentacao = new JMenuItem("Documentação");
+		mntmDocumentacao.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TelaDocumentacao tela = new TelaDocumentacao();
+				tela.setVisible(true);
+				desktopPane.add(tela);
+			}
+		});
+		Registros.add(mntmDocumentacao);
+		
+		JMenuItem mntmVagas = new JMenuItem("Vagas");
+		mntmVagas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Vagas tela = new Vagas();
+				tela.setVisible(true);
+				desktopPane.add(tela);
+			}
+		});
+		Registros.add(mntmVagas);
+		
+		JMenuItem mntmRelatorios = new JMenuItem("Relatórios");
+		Registros.add(mntmRelatorios);
+		
+		JMenuItem mntmEstagio = new JMenuItem("Estágio");
+		Registros.add(mntmEstagio);
+		
+		JMenuItem mntmContato = new JMenuItem("Contato");
+		Registros.add(mntmContato);
+		
+		JMenuItem mntmAjuda = new JMenuItem("Ajuda");
+		Registros.add(mntmAjuda);
+		
+		JMenu mnAjuda = new JMenu("Ajuda");
+		menuBar.add(mnAjuda);
+		
+		JMenuItem mntmSobre = new JMenuItem("Sobre");
+		mnAjuda.add(mntmSobre);
+	}
+
+}
